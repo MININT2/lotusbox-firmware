715cb16de504ccd9c4fde286c0cf01281ca7db01
minty <minint.dsk@gmail.com>
2025-01-25 12:55:40 -0600

    metadata

--- crystal/src/objects/binary.rs
+++ crystal/src/objects/binary.rs
@@ -1,13 +1,25 @@
 use xxhash_rust::xxh3::xxh3_64;
 
+pub enum MetadataFields {
+    Name,
+    Date,
+    Time
+}
+pub struct Metadata {
+    fields: Vec<MetadataFields>
+}
 pub struct RawData {
     data: Vec<u8>, // the raw binary data
-    meta: Vec<u64>, // metadata map of regions
-    hash: Vec<u64> // hash of the data inside
+    meta: Vec<Metadata>, // metadata map of regions
+    hash: u64 // hash of the data inside
 }
 impl RawData {
     pub fn read(&self, index: u64) {
 
     }
-    pub fn write(&self, data: Vec<u8>, )
+    pub fn write(&mut self, data: Vec<u8>, meta: Vec<Metadata>) {
+        self.data = data;
+        self.hash = xxh3_64(self.data.as_slice());
+        self.meta = meta;
+    }
 }
